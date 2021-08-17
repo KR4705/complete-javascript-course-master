ab21b8008b74e9d99f5732f4fa909c9f5256d254
KR4705 <gamerdude9@gmail.com>
2021-08-17 19:57:43 +0530

    exercise 1 completed learned about Destructuring Objects and Arrays Rest and spread Operators Using rest and spread operators to define multi Args Functions

---
+++ 09-Data-Structures-Operators/starter/script.js
@@ -0,0 +1,133 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+//coding challenge 1
+//data
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//task 1
+//Create one player array for each team (variables 'players1' and 'players2')
+
+const [players1, players2] = game.players;
+// console.log(players1[0]);
+// console.log(game.players);
+
+//The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+
+const [gk, ...fieldPlayers] = game.players[0];
+// console.log(gk);
+// console.log(fieldPlayers);
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players
+
+// const allPlayers = [...game.players[0], ...game.players[1]];
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+//During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+// Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2'
+
+// const { team1, x: draw, team2 } = game.odds; // Powerful buy confusing
+const {
+  odds: { team1, x: draw, team2 },
+} = game; //example of nested deconstruction from left hand side I have written alternative which works same way.
+
+console.log(team1, draw, team2);
+
+// Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+
+function printGoals(...args) {
+  console.log(...args, args.length);
+}
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
+
+// The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator
+
+//tricky but useful step
+team1 < team2 &&
+  console.log(`team1 has higher chance of winning, ${team1},${team2}`);
+team2 < team1 &&
+  console.log(`team2 has higher chance of winning, ${team1},${team2}`);
